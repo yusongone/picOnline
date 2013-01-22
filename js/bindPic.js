19ea6aa873f8c68e78e5169fa4980952b3583672
yusong <yusongone@gmail.com>
2013-01-22 20:53:14 +0800

    增加提交

--- js/bindPic.js
+++ js/bindPic.js
@@ -1,19 +1,114 @@
-var pageSpace={};
+var pageSpace;
 $(document).ready(function(){
 	pageSpace.con=$("#picList");
-	pageSpace.renderPic();
+	pageSpace._btn_fileUp=$("#fileUp");
+	pageSpace.bindEvent();
 });
 
-pageSpace.renderPic=function(){
-	var p=$("<div/>",{"class":"pic"});	
-	var img=$("<img/>",{"class":"img-polaroid img","src":"/css/img/002.jpg"})
-	var alphaDiv=$("<div/>",{"class":"alphaDiv"});
-	var infoDiv=$("<div/>",{"class":"infoDiv"});
-		var text=$("<div/>",{"class":"progressText","text":"0%"});
-		var bar=$("<div/>",{"class":"progressBar"});
-		infoDiv.append(text,bar);
-	var closeDiv=$("<div/>",{"class":"closeDiv"});
-		var ii=$("<i/>",{"class":"icon-remove"});
-		
-	pageSpace.con.append(p.append(img,alphaDiv,infoDiv,closeDiv.append(ii)));
-}
+pageSpace=pageSpace||(function(){
+	var pageSpaceTemp={};
+	/*
+	 * bind 事件;
+	 * */
+	pageSpaceTemp.bindEvent=function(){
+		pageSpace._btn_fileUp.change(function(evt){
+			var files=$(this)[0].files;
+			var fileupload=pageSpaceTemp.FileUpload;
+			for(var i=0,l=files.length;i<l;i++){
+				var img="image/jpeg image/png";
+				if(img.indexOf(files[i].type)!=-1){
+					var fu=new fileupload(files[i]);
+				};
+			}
+		});
+		document.addEventListener("drop",function(e){
+			var files=e.dataTransfer.files;
+			var fileupload=pageSpaceTemp.FileUpload;
+			for(var i=0,l=files.length;i<l;i++){
+				var fu=new fileupload(files[i]);
+			}
+			e.preventDefault();
+		},false);
+	};
+	/*
+	 *
+	 * */
+	function FileUpload(file){
+		this.file=file;
+		var ul=pageSpaceTemp.Uploader;
+		this.xhr=new XMLHttpRequest();
+		this.UI=new ul(file,this.xhr);
+		this.bindEvent();
+		this.send();
+	};
+	FileUpload.prototype.send=function(){
+		var fd=new FormData();
+			fd.append("file",this.file,true);
+		this.xhr.open("POST","/sample/files");
+		this.xhr.setRequestHeader("X-Requested-With","XMLHttpRequest");
+		this.xhr.send(fd);
+	}
+	FileUpload.prototype.bindEvent=function(fun){
+		var that=this;
+		var xhr=this.xhr;
+		xhr.addEventListener("load",fun,false);	
+		xhr.addEventListener("error",fun,false);	
+		xhr.addEventListener("abort",function(){
+			alert(that.file.name);
+		},false);	
+		xhr.upload.addEventListener("progress",function(e){
+			if(e.lengthComputable){
+				var loaded=Math.ceil((e.loaded/e.total)*100);
+			}
+			that.UI.bar.css({"width":loaded+"%"});
+			that.UI.text.text(loaded+"%");
+			
+		},false);
+	};
+
+	pageSpaceTemp.FileUpload=FileUpload;
+
+
+	/*
+	 *uploader
+	 * */
+	function Uploader(file,xhr){
+		var that=this;
+			this.xhr=xhr;
+		var imgRead=new FileReader();
+			imgRead.readAsDataURL(file);
+			that.initUI({"name":file.name});
+			imgRead.onload=function(f){
+					var img="image/jpeg image/png";
+					if(img.indexOf(file.type)!=-1){
+						that.UIimg.attr("src",f.srcElement.result);
+					};
+			}
+	}
+	Uploader.prototype.initUI=function(json){
+		var that=this;
+		var p=$("<div/>",{"class":"img-polaroid pic"});	
+		var imgBox=$("<div/>",{"class":"imgBox"})
+			this.UIimg=$("<img/>",{"class":"image","src":json.src})
+			var alphaDiv=$("<div/>",{"class":"alphaDiv"});
+			var infoDiv=$("<div/>",{"class":"infoDiv"});
+				this.text=$("<div/>",{"class":"progressText","text":"0%"});
+				this.bar=$("<div/>",{"class":"progressBar"});
+				infoDiv.append(this.text,this.bar);
+			imgBox.append(this.UIimg,alphaDiv,infoDiv);
+		var closeDiv=$("<div/>",{"class":"closeDiv"});
+			var ii=$("<i/>",{"class":"icon-remove"});
+		this.UIname=$("<div/>",{"class":"name","text":json.name});
+			p.append(imgBox,closeDiv.append(ii),this.UIname)
+		pageSpace.con.append(p);
+			console.dir(that.xhr);
+			closeDiv.click(function(){
+				if(confirm("你确定要取消此图片？")){
+					that.xhr.abort();
+				}
+			});
+	}
+	pageSpaceTemp.Uploader=Uploader;
+
+	return pageSpaceTemp;
+})();
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var pageIndex={};
 $(document).ready(function(){
 	pageIndex._btn_createOrder=$("#createOrder");
+	pageIndex._btn_newUser=$("#newUser");
 	pageIndex._div_hideArea=$("#hideCon");
 	pageIndex._table_order=$("#order");
 	pageIndex.hideArea=publicJs.createHideArea(pageIndex._div_hideArea);
@@ -8,55 +8,151 @@ $(document).ready(function(){
 	//pageIndex.selectMoudleByAjax();
 });
 
+var pageIndex=pageIndex||(function(){
+	tempSpace={};
+	//绑定页面按钮事件
+	tempSpace.bind=function(){
+			var that=this;
+			this._btn_createOrder.click(function(){
+				//that.initCreateOrderUI();
+				var div=$("<div/>",{});
+				var _table_userList=$("<table/>",{"class":"table table-hover"});
+				var createBox=that.initNewUserUI();
+				div.append(createBox,_table_userList);
+				pageIndex.hideArea.open(div);
+				that.getUserList(_table_userList);
+			});
 
+		//绑定新建用户按钮
+			that._btn_newUser.click(function(){
+				that.initNewUserUI();
+			});
+		}
 
-
-pageIndex.bind=function(){
-	var that=this;
-	this._btn_createOrder.click(function(){
-			if(pageIndex.hideArea.getState()=="show"){
-				pageIndex.hideArea.hide();	
-				return false;
-			}
-		var div=$("<div/>",{"class":"form-inline center","style":"width:340px;"});
-			var name=$("<input/>",{"type":"text","class":"input-small","placeholder":"姓名"});
-			var id=$("<input/>",{"type":"text","class":"input-small","placeholder":"预留帐号","style":"margin-left:5px;"});
-			var ok=$("<a/>",{"class":"btn btn-primary","text":"创建","style":"margin-left:10px;"});
-			var clean=$("<a/>",{"class":"btn","text":"取消","style":"margin-left:5px;"});
-			div.append(name,id,ok,clean);
-			pageIndex.hideArea.open(div);
-				ok.click(function(){
-					that.addOrderByAjax({
-							"name":name.val(),
-							"id":id.val(),
-							"callback":function(){
-									that.hideArea.close()
-							}
+	//初始化创建订单界面
+	tempSpace.initCreateOrderUI=function(){
+				var that=this;
+				var div=$("<div/>",{"class":"form-inline center","style":"width:340px;"});
+				var name=$("<input/>",{"type":"text","class":"input-small","placeholder":"姓名"});
+				var id=$("<input/>",{"type":"text","class":"input-small","placeholder":"预留帐号","style":"margin-left:5px;"});
+				var ok=$("<a/>",{"class":"btn btn-primary","text":"创建","style":"margin-left:10px;"});
+				var clean=$("<a/>",{"class":"btn","text":"取消","style":"margin-left:5px;"});
+				div.append(name,id,ok,clean);
+				pageIndex.hideArea.open(div);
+					clean.click(function(){
+						that.hideArea.close();
 					});
-				});
-				clean.click(function(){
-					that.hideArea.close();
-				});
-	});
-};
+	
+	};
+	//初始化新建用户界面
+	tempSpace.initNewUserUI=function(){
+				var that=this;
+				var div=$("<div/>",{"class":"form-inline center","style":"width:340px;"});
+				var name=$("<input/>",{"type":"text","class":"input-small","placeholder":"昵称(备注)"});
+				var id=$("<input/>",{"type":"text","class":"input-small","placeholder":"预留帐号","style":"margin-left:5px;"});
+				var ok=$("<a/>",{"class":"btn btn-primary","text":"创建","style":"margin-left:10px;"});
+				var clean=$("<a/>",{"class":"btn","text":"取消","style":"margin-left:5px;"});
+				div.append(id,name,ok,clean);
+			//	that.hideArea.open(div);
+					ok.click(function(){
+						that.createUser({
+								"name":name.val(),
+								"account":id.val(),
+								"callback":function(){
+										that.hideArea.close()
+								}
+						});
+					});
+				return div;
+	};
+	tempSpace.getUserList=function(table){
+		var that=this;
+		$.ajax({
+			type:"post",
+			url:"/user/getUserList",
+			data:{"pageNum":0,"pageSize":20},
+			dataType:"json",
+			cache:false,
+			success:function(data){
+				that.renderUserList(data.data,table);	
+			}
+		});
+	}
+	tempSpace.renderUserList=function(ary,table){
+		var that=this;
+		for(var i=0,l=ary.length;i<l;i++){
+				var tr=$("<tr/>",{});	
+				var td1=$("<td/>",{"text":i});
+				var td2=$("<td/>",{"text":ary[i].name});
+				var td3=$("<td/>",{"text":"2012-3-15"});
+				var td4=$("<td/>",{"text":ary[i].accounts});
+				var td5=$("<td/>",{});
+					var moudle=$("<a/>",{"text":"创建订单"});
+				tr.append(td1,td2,td3,td4,td5.append(moudle));
+				table.append(tr);	
+				(function(){
+					var json=ary[i];
+					moudle.click(function(){
+						that.addOrder(json);	
+					});
+				})();
+		}	
+	}
 
-pageIndex.addOrderByAjax=function(json){
-	var that=this;
-	var tr=$("<tr/>",{});	
-		var td1=$("<td/>",{"text":"2"});
-		var td2=$("<td/>",{"text":json.name});
-		var td3=$("<td/>",{"text":"2012-3-15"});
-		var td4=$("<td/>",{"text":"新创建"});
-		var td5=$("<td/>",{});
-			var moudle=$("<a/>",{"text":"管理模板","href":"/site/bindProduct","target":"_blank"});
-			var pic=$("<a/>",{"text":"管理图片"});
-	tr.append(td1,td2,td3,td4,td5.append(moudle,"|",pic));
-	this._table_order.append(tr);	
-	json.callback();
-	return false;
-		var td1=$("<td/>",{"text":"2"});
-	$.ajax({});
-};
+	tempSpace.createUser=function(json){
+		$.ajax({
+			type:"post",
+			url:"/user/newUser",
+			data:{"name":json.name,"account":json.account},
+			dataType:"json",
+			cache:false,
+			success:function(data){
+				alert(data);
+			}
+		});
+		return false;
+		var that=this;
+		json.callback();
+	};
+	tempSpace.addOrder=function(json){
+		$.ajax({
+			type:"post",
+			url:"/order/createOrder",
+			data:{"userId":json.id},
+			dataType:"json",
+			cache:false,
+			success:function(data){
+				alert(data);
+			}
+		});
+		return false;
+				var tr=$("<tr/>",{});	
+				var td1=$("<td/>",{"text":i});
+				var td2=$("<td/>",{"text":ary[i].name});
+				var td3=$("<td/>",{"text":"2012-3-15"});
+				var td4=$("<td/>",{"text":ary[i].accounts});
+				var td5=$("<td/>",{});
+					var moudle=$("<a/>",{"text":"管理模板","href":"/site/bindProduct","target":"_blank"});
+					var pic=$("<a/>",{"text":"管理图片"});
+				tr.append(td1,td2,td3,td4,td5.append(moudle,"|",pic));
+				table.append(tr);	
+
+	}
+	tempSpace.checkAccountByAjax=function(json){
+		$.ajax({
+			type:"post",
+			url:"/user/searchUserByAccount",
+			data:{"name":json.name,"account":json.account},
+			dataType:"json",
+			cache:false,
+			success:function(data){
+				alert(data);
+			}
+		});
+	}
+
+	return tempSpace;
+})();
 
 
 
